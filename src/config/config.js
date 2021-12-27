3a5d2a35ec426dba116bc9d493a0285c41433edc
Ajay <yatmadevybk2790@gmail.com>
2021-12-27 16:00:27 +0530

    first commit

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,6 @@
 // export const ip = "http://34.93.5.144:4000";
 //export const ip = "https://api.eazr.in";
 // export const ip = "http://3.110.166.216:4000";
-export const ip = "https://apin.eazr.in";
+export const ip = "https://api.eazr.in";
 // http://10.212.7.250:4000/user/transactions?id=618b7ed63036016860247988
 //test ajay
